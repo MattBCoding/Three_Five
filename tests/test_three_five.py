bb3d477c19313ff23b35e021c22c00919e85c16a
Matthew Bodden <mattbodden@googlemail.com>
2023-10-18 10:53:28 +0100

    Add test for multiples of five

--- tests/test_three_five.py
+++ tests/test_three_five.py
@@ -13,6 +13,14 @@ class TestThreeFive(unittest.TestCase):
         self.assertEqual(three_five(12), "Three")
         self.assertEqual(three_five(18), "Three")
 
+    def test_multiples_of_five(self):
+        """Test that multiples of five print 'Five'"""
+        self.assertEqual(three_five(5), "Five")
+        self.assertEqual(three_five(10), "Five")
+        self.assertEqual(three_five(20), "Five")
+        self.assertEqual(three_five(25), "Five")
+        self.assertEqual(three_five(35), "Five")
+
 
 if __name__ == "__main__":
     unittest.main()
